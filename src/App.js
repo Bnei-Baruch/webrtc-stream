fafb6c6d85e6a5bb5d69d136bcc812f280b744ab
amnonbb <amnonbb@gmail.com>
2025-07-14 07:35:14 +0300

    Update keycloak package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-// import MainPage from "./apps/MainPage";
+import MainPage from "./apps/MainPage";
 // import CroomStream from "./apps/CroomStream";
 // import StreamCapture from "./apps/StreamCapture";
 // import DualOut from "./apps/DualOut";
 // import CaptureMonitor from "./apps/CaptureMonitor";
 // import LocalStream from "./apps/LocalStream";
 // import IngestMonitor from "./apps/IngestMonitor";
-import AdminStreaming from "./apps/AdminStreaming";
+// import AdminStreaming from "./apps/AdminStreaming";
 // import MqttStreaming from "./apps/MqttStreaming";
 // import HttpStreaming from "./apps/HttpStreaming";
 // import FullScreenStream from "./apps/FullScreenStream";
@@ -18,8 +18,8 @@ class App extends Component {
   render() {
     return (
         <Fragment>
-            {/*<MainPage />*/}
-           <AdminStreaming/>
+            <MainPage />
+           {/*<AdminStreaming/>*/}
             {/*<MqttStreaming />*/}
             {/*<HttpStreaming />*/}
             {/*<FullScreenStream />*/}
